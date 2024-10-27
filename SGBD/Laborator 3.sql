bf17031ff7b229d47aca44de458150430233035e
AndreiFishe <andrei.opran@icloud.com>
2024-10-27 21:45:34 +0200

    SGBD tema laborator 3

--- SGBD/Laborator 3.sql
+++ SGBD/Laborator 3.sql
@@ -114,26 +114,96 @@ END;
 SET VERIFY ON
 
 
+
 --8
+DECLARE
+v_cod employees.employee_id%TYPE:=&p_cod;
+v_bonus NUMBER(8);
+v_salariu_anual NUMBER(8);
+BEGIN
+     SELECT salary*12 INTO v_salariu_anual
+     FROM employees 
+     WHERE employee_id = v_cod;
+     CASE 
+     WHEN v_salariu_anual>=200001
+     THEN v_bonus:=20000;
+     WHEN v_salariu_anual BETWEEN 100001 AND 200000
+     THEN v_bonus:=10000;
+     ELSE v_bonus:=5000;
+END CASE;
+DBMS_OUTPUT.PUT_LINE('Bonusul este ' || v_bonus);
+EXCEPTION
+    when no_data_found then
+        dbms_output.put_line('Nu a fost gasit niciun angajat cu id-ul introdus!');
+END;
+
+
+
+-- 9
+DEFINE p_cod_sal= 200
+DEFINE p_cod_dept = 80
+DEFINE p_procent =20
+DECLARE
+v_cod_sal emp_***.employee_id%TYPE:= &p_cod_sal;
+v_cod_dept emp_***.department_id%TYPE:= &p_cod_dept;
+v_procent NUMBER(8):=&p_procent;
+BEGIN
+     UPDATE emp_***
+     SET department_id = v_cod_dept, 
+     salary=salary + (salary* v_procent/100)
+     WHERE employee_id= v_cod_sal;
+     IF SQL%ROWCOUNT =0 THEN 
+     DBMS_OUTPUT.PUT_LINE('Nu exista un angajat cu acest cod');
+     ELSE DBMS_OUTPUT.PUT_LINE('Actualizare realizata');
+     END IF;
+END;
+/
+
+
+----
+
+DECLARE 
+    contor NUMBER(6) := 1; 
+    v_data DATE; 
+    maxim NUMBER(2) := LAST_DAY(SYSDATE)-SYSDATE; 
+    
+    BEGIN 
+        LOOP v_data := sysdate+contor; 
+            INSERT INTO zile_*** 
+            VALUES (contor,v_data,to_char(v_data,'Day')); 
+            contor := contor + 1; 
+            EXIT WHEN contor > maxim; 
+        END LOOP; 
+    END; 
+    /
+
+
+
+
+-- 10
+create table zile_ao (
+
+    id number,
+
+    data date,
+
+    nume_zi varchar2(30)
+
+)
+ 
+
+
+
+-- 13
+DECLARE i POSITIVE:=1; max_loop CONSTANT POSITIVE:=10; BEGIN LOOP i:=i+1; IF i>max_loop THEN DBMS_OUTPUT.PUT_LINE('in loop i=' || i); GOTO urmator; END IF; END LOOP; <> i:=1; DBMS_OUTPUT.PUT_LINE('dupa loop i=' || i); END;
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- tema
+-- fara E1, E6
+-- E2,E3,E4,E5
 
 
 
