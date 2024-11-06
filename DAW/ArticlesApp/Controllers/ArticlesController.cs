935585197c7cc21af41aa0a2bd87e26353f95f02
AndreiFishe <andrei.opran@icloud.com>
2024-11-06 17:54:49 +0200

    upload

---
+++ DAW/ArticlesApp/Controllers/ArticlesController.cs
@@ -0,0 +1,24 @@
+﻿using ArticlesApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticlesApp.Controllers
+{
+    public class ArticlesController : Controller
+    {
+
+        private readonly ApplicationDbContext db; // readonly pt ca este o referinta
+        public ArticlesController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            var articles = db.Articles.Include("Category"); // include face join cu tabelele asociate; lambda expression
+            ViewBag.Articles = articles;
+
+            return View();
+        }
+    }
+}
---
+++ DAW/ArticlesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ArticlesApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticlesApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // daca nu adaug astea nu apar tabelele in baza de date
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
