5e9e8b19af5083f2f4eb54a596b773b6b279a079
Andrei <andrei.opran@icloud.com>
2024-11-15 18:24:58 +0200

    upload

--- TestariCPP/TestariCPP/TestariCPP.cpp
+++ TestariCPP/TestariCPP/TestariCPP.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
@@ -6,37 +7,33 @@ using namespace std;
 
 class Solution {
 public:
-	bool containsDuplicate(vector<int>& nums) {
+	string longestPalindrome(string s) {
+		
+		int len = s.length();
 
-		if (nums.size() == 2 && nums[0] == nums[1])
-			return true;
-
-		sort(nums.begin(), nums.end());
-
-		bool existaDuplicate = false;
-		for (int i = 1; i < nums.size() - 1; i+=2)
+		for (int i = 0; i < len - 1; i++)
 		{
-			//cout << nums[i - 1] << ' ' << nums[i] << ' ' << nums[i + 1] << '\n';
-			if (nums[i] == nums[i - 1] || nums[i] == nums[i + 1])
-				return true;
-			if (nums.size() % 2 == 0)
+			for (int j = i + 1; j < len; j++)
 			{
-				if (nums[i] == nums[i + 1])
-					return true;
+
 			}
 		}
-		if (nums[nums.size() - 1] == nums[nums.size() - 2] && nums.size() > 3)
-			return true;
-		return existaDuplicate;
+	}
+
+	bool palindrome(string s)
+	{
+		string aux = s;
+		reverse(aux.begin(), aux.end());
+		return aux == s;
 	}
 };
 
 
 int main()
 {
-	vector<int> nums = {0 };
+	string s = "babad";
 	Solution sol;
-	cout << sol.containsDuplicate(nums);
-    return 0;
+	cout << sol.longestPalindrome(s);
+	return 0;
 }
 
