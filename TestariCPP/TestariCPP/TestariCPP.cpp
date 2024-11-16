d34514a93bb5395d8f4ff9ff281295ea316c4148
Andrei <andrei.opran@icloud.com>
2024-11-16 17:09:41 +0200

    upload

--- TestariCPP/TestariCPP/TestariCPP.cpp
+++ TestariCPP/TestariCPP/TestariCPP.cpp
@@ -3,37 +3,52 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <utility>
 using namespace std;
+
+/*
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+
+*/
 
 class Solution {
 public:
-	string longestPalindrome(string s) {
-		
-		int len = s.length();
+    int getMaxTraffic(int start[], int end[], int n)
+    {
+        
+        const int size = n + 1;
 
-		for (int i = 0; i < len - 1; i++)
-		{
-			for (int j = i + 1; j < len; j++)
-			{
+       
+        vector<bool> connected(size, false);
 
-			}
-		}
-	}
+        /*unordered_map<int, pair<int, int>> map;
+        for (int i = 0; i < n; i++)
+        {
+            map[i] = make_pair(start[i], end[i]);
+        }*/
 
-	bool palindrome(string s)
-	{
-		string aux = s;
-		reverse(aux.begin(), aux.end());
-		return aux == s;
-	}
+        int time = 1;
+        int cont = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if()
+        }
+        
+    }
 };
-
 
 int main()
 {
-	string s = "babad";
-	Solution sol;
-	cout << sol.longestPalindrome(s);
+    int start[] = { 1,6,2,9 };
+    int end[] = { 8,7,6,10 };
+    int n = 4;
+    Solution sol;
+    cout << sol.getMaxTraffic(start, end, n);
 	return 0;
 }
-
