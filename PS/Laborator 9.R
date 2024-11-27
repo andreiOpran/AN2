b604e601d4d9d1a3755656eaf84d16ebf5e39ed5
AndreiFishe <andrei.opran@icloud.com>
2024-11-27 02:58:32 +0200

    upload

--- PS/Laborator 9.R
+++ PS/Laborator 9.R
@@ -24,3 +24,36 @@ View(sort(Parade2005$earnings[Parade2005$celebrity == "yes"]))
 ex1 = c(1,2,3,4,5,6,7,8,1000)
 mean(ex1)
 median(ex1)
+
+
+# problema 2
+# distributia hipergeometrica
+?dhyper
+pb2 = dhyper(0:3, 3, 2, 3)
+pb2
+
+# Fx(49) = P(X <= 49), phyper(49,...)
+# problema 3
+?phyper
+pb3 = phyper(49, 200, 20000 - 200, 1000, lower.tail = FALSE)
+pb3
+
+# problema 4.1
+# distributia binomiala
+?dbinom
+pb4_1 = dbinom(0, 3, 0.3)
+pb4_1
+
+
+# problema 4.2
+# distributia hipergeometrica
+?dhyper
+# pb4_2 = dhyper(, 8, 22, 3)
+pb4_2
+
+# problema 5
+dbinom(0, size = 1e4, prob = 3/1e5)
+dpois(0, lambda = 0.3)
+1-exp(-0.3)*1.3
+
+
