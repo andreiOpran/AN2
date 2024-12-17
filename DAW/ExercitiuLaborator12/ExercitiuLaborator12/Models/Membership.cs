a0cfa52955f59361f396f04af31a58baa03def5a
andreiOpran <andrei.opran@icloud.com>
2024-12-17 22:17:55 +0200

    upload

---
+++ DAW/ExercitiuLaborator12/ExercitiuLaborator12/Models/Membership.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExercitiuLaborator12.Models
+{
+    public class Membership
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Titlul abonamentului este obligatoriu.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Titlul abonamentului trebuie să aibă între 3 și 100 de caractere.")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Valoarea abonamentului este obligatorie.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Valoarea abonamentului trebuie să fie un număr pozitiv.")]
+        public int? Valoare { get; set; }
+
+        [Required(ErrorMessage = "Data emiterii abonamentului este obligatorie.")]
+        public DateTime? DataEmitere { get; set; }
+
+        [Required(ErrorMessage = "GymId este obligatoriu.")]
+        public int? GymId { get; set; }
+
+        [Required(ErrorMessage = "Gym este obligatoriu.")]
+        public virtual Gym? Gym { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem>? Gyms { get; set; }
+    }
+}
