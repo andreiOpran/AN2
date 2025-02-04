dfe695d83c6cc4b202ba30e5d553fe3d9f43a3c4
andreiOpran <andrei.opran@icloud.com>
2025-02-05 00:44:30 +0200

    Upload

--- PF/.1_Materiale examen/Drive/PF2024_public_lab/L05/PF2024-Lab5.hs
+++ PF/.1_Materiale examen/Drive/PF2024_public_lab/L05/PF2024-Lab5.hs
@@ -1,20 +1,35 @@
+sumaPatrateImpare :: [Int] -> Int
+sumaPatrateImpare l = foldr (+) 0 $ map (^2) $ filter odd l
+-- sumaPatrateImpare l = foldr ((+) . (^2)) 0 (filter odd l)
+
+toateTrue :: [Bool] -> Bool
+toateTrue l = foldr (&&) True l
+
 allVerifies :: (Int -> Bool) -> [Int] -> Bool
-allVerifies = undefined
+allVerifies f l = foldl (&&) True $ map f l
 
 anyVerifies :: (Int -> Bool) -> [Int] -> Bool
-anyVerifies = undefined
+anyVerifies f l = foldl (||) False $ map f l
+
+mapFoldr :: (a -> b) -> [a] -> [b]
+mapFoldr f l = foldr (\x acc -> f x : acc) [] l
+
+filterFoldr :: (a -> Bool) -> [a] -> [a]
+filterFoldr f l = foldr(\x acc -> if f x then x : acc else acc) [] l
 
 listToInt :: [Integer]-> Integer
-listToInt = undefined
+listToInt l = foldl (\acc x -> acc * 10 + x) 0 l
 
 rmChar :: Char -> String -> String
-rmChar = undefined
+rmChar c l = reverse $ foldl (\acc x -> if x /= c then x : acc else acc) "" l
 
 rmCharsRec :: String -> String -> String
-rmCharsRec = undefined
+rmCharsRec c [] = []
+rmCharsRec [] l = l
+rmCharsRec (c:cs) l = rmCharsRec cs (rmChar c l)
 
 rmCharsFold :: String -> String -> String
-rmCharsFold = undefined
-    
+rmCharsFold cs l = foldr (rmChar) l cs
+
 myUnzip :: [(a, b)] -> ([a], [b])
 myUnzip = undefined
