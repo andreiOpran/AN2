8860a4238259853043c9a814683d5e2c7e014337
andreiOpran <andrei.opran@icloud.com>
2025-02-05 22:59:11 +0200

    Upload

--- PF/.1_Materiale examen/Drive/PF2024_public_lab/L09/PF2024-L09.hs
+++ PF/.1_Materiale examen/Drive/PF2024_public_lab/L09/PF2024-L09.hs
@@ -1,9 +1,9 @@
 data Tree = Empty  -- arbore vid
    | Node Int Tree Tree Tree -- arbore cu valoare de tip Int in radacina si 3 fii
       
--- extree :: Tree
--- extree = Node 4 (Node 5 Empty Empty Empty) 
---                 (Node 3 Empty Empty (Node 1 Empty Empty Empty)) Empty
+extree :: Tree
+extree = Node 4 (Node 5 Empty Empty Empty) 
+                (Node 3 Empty Empty (Node 1 Empty Empty Empty)) Empty
 
 class ArbInfo t where
   level :: t -> Int -- intoarce inaltimea arborelui; pt un arbore vid
@@ -16,6 +16,22 @@ class ArbInfo t where
 -- 13
 -- nrFrunze extree
 -- 2
+
+instance ArbInfo Tree where
+  level :: Tree -> Int
+  level Empty = 0
+  level (Node x left mid right) = 1 + maximum [level left, level mid, level right]
+
+  sumval :: Tree -> Int
+  sumval Empty = 0
+  sumval (Node x left mid right) = x + sumval left + sumval mid + sumval right
+
+  nrFrunze :: Tree -> Int
+  nrFrunze Empty = 0
+  nrFrunze (Node x left mid right)
+    | level (Node x left mid right) == 1 = 1
+    | otherwise = nrFrunze left + nrFrunze mid + nrFrunze right
+
 
 
 class Scalar a where
@@ -32,4 +48,106 @@ class (Scalar a) => Vector v a where
   addv :: v a -> v a -> v a -- adunare vector
   smult :: a -> v a -> v a  -- inmultire cu scalare
   negatev :: v a -> v a -- negare vector
+
+-- instantiati calsa Scalar folosindu-va de tipuri primitive
+-- am ales rational pentru ca se pliaza pe vectori, daca am fi ales INT era ciudat la recips
+instance Scalar Rational where
+  zero :: Rational
+  zero = 0
+  
+  one :: Rational
+  one = 1
+
+  adds :: Rational -> Rational -> Rational
+  adds = (+)
+
+  mult :: Rational -> Rational -> Rational
+  mult = (*)
+
+  negates :: Rational -> Rational
+  negates = negate
+
+  recips :: Rational -> Rational
+  recips = recip
+
+
+-- de la copilot
+-- instance Scalar Int where
+--   zero :: Int
+--   zero = 0
+  
+--   one :: Int
+--   one = 1
+  
+--   adds :: Int -> Int -> Int
+--   adds = (+)
+  
+--   mult :: Int -> Int -> Int
+--   mult = (*)
+  
+--   negates :: Int -> Int
+--   negates = negate
+
+--   recips :: Int -> Int
+--   recips x = error "No reciprocal for Int"
+
+-- instance Scalar Double where
+--   zero :: Double
+--   zero = 0.0
+  
+--   one :: Double
+--   one = 1.0
+  
+--   adds :: Double -> Double -> Double
+--   adds = (+)
+  
+--   mult :: Double -> Double -> Double
+--   mult = (*)
+  
+--   negates :: Double -> Double
+--   negates = negate
+  
+--   recips :: Double -> Double
+--   recips = (1 /)
+
+
+-- scrieti 2 instante ale clasei Vector pentru a reprezenta vectori bidimensionali si tridimensionali
+data V2D a = V2D a a deriving Show
+
+instance (Scalar a) => Vector V2D a where
+  zerov :: Scalar a => V2D a
+  zerov = V2D zero zero
+  
+  onev :: Scalar a => V2D a
+  onev = V2D one one
+
+  addv :: Scalar a => V2D a -> V2D a -> V2D a
+  addv (V2D x1 y1) (V2D x2 y2) = V2D (adds x1 x2) (adds y1 y2)
+
+  smult :: Scalar a => a -> V2D a -> V2D a
+  smult a (V2D x y) = V2D (mult a x) (mult a y)
+
+  negatev :: Scalar a => V2D a -> V2D a
+  negatev (V2D x y) = V2D (negates x) (negates y)
+
+data V3D a = V3D a a a deriving Show
+
+instance (Scalar a) => Vector V3D a where
+  zerov :: Scalar a => V3D a
+  zerov = V3D zero zero zero
+
+  onev :: Scalar a => V3D a
+  onev = V3D one one one
+
+  addv :: Scalar a => V3D a -> V3D a -> V3D a
+  addv (V3D x1 y1 z1) (V3D x2 y2 z2) = V3D (adds x1 x2) (adds y1 y2) (adds z1 z2)
+
+  smult :: Scalar a => a -> V3D a -> V3D a
+  smult a (V3D x y z) = V3D (mult a x) (mult a y) (mult a z)
+
+  negatev :: Scalar a => V3D a -> V3D a
+  negatev (V3D x y z) = V3D (negates x) (negates y) (negates z)
+
+myV3D :: V3D Rational
+myV3D = zerov
 
--- PF/.1_Materiale examen/Drive/PF2024_public_lab/L10/PF2024-L10.hs
+++ PF/.1_Materiale examen/Drive/PF2024_public_lab/L10/PF2024-L10.hs
@@ -9,6 +9,8 @@ data Prop
   | Not Prop
   | Prop :|: Prop
   | Prop :&: Prop
+  | Prop :->: Prop
+  | Prop :<->: Prop
   deriving (Eq, Read)
 infixr 2 :|:
 infixr 3 :&:
@@ -17,17 +19,26 @@ p1 :: Prop
 p1 = (Var "P" :|: Var "Q") :&: (Var "P" :&: Var "Q")
 
 p2 :: Prop
-p2 = undefined
+p2 = (Var "P" :|: Var "Q") :&: (Not (Var "P") :&: Not (Var "Q"))
 
 p3 :: Prop
-p3 = undefined
+p3 = (Var "P" :&: (Var "Q" :|: Var "R")) :&: (Not (Var "P") :|: Not (Var "Q")) :&: (Not (Var "P") :|: Not (Var "R"))
 
+-- faceti tipul Prop instanta a clasei de tipuri Show
 instance Show Prop where
-  show = undefined
- 
+  show (Var x) = x
+  show T = "T"
+  show F = "F"
+  show (x :&: y) = "(" ++ show x ++ "&" ++ show y ++ ")"
+  show (x :|: y) = "(" ++ show x ++ "|" ++ show y ++ ")"
+  show (Not x) = "(" ++ "~" ++ show x ++ ")"
+  show (x :->: y) = "(" ++ show x ++ "->" ++ show y ++ ")"
+  show (x :<->: y) = "(" ++ show x ++ "<->" ++ show y ++ ")"
 test_ShowProp :: Bool
 test_ShowProp =
     show (Not (Var "P") :&: Var "Q") == "((~P)&Q)"
+
+
 
 type Env = [(Nume, Bool)]
 
@@ -35,20 +46,39 @@ impureLookup :: Eq a => a -> [(a,b)] -> b
 impureLookup a = fromJust . lookup a
 
 eval :: Prop -> Env -> Bool
-eval = undefined
- 
+eval (Var name) env = impureLookup name env
+eval T _ = True
+eval F _ = False
+eval (Not p) env = not (eval p env)
+eval (p :|: q) env = eval p env || eval q env
+eval (p :&: q) env = eval p env && eval q env
+eval (p :->: q) env = not (eval p env) || eval q env
+eval (p :<->: q) env = eval (p :->: q) env == eval (q :->: p) env
+
 test_eval = eval  (Var "P" :|: Var "Q") [("P", True), ("Q", False)] == True
 
+
 variabile :: Prop -> [Nume]
-variabile = undefined
- 
+variabile (Var x) = [x]
+variabile T = []
+variabile F = []
+variabile (Not p) = variabile p
+variabile (p :&: q) = nub (variabile p ++ variabile q)
+variabile (p :|: q) = nub (variabile p ++ variabile q)
+variabile (p :->: q) = nub (variabile p ++ variabile q)
+variabile (p :<->: q) = nub (variabile p ++ variabile q)
+
 test_variabile =
   variabile (Not (Var "P") :&: Var "Q") == ["P", "Q"]
 
-envs :: [Nume] -> [Env]
-envs = undefined
- 
-test_envs = 
+
+
+envs :: [Nume] -> [[(Nume, Bool)]]
+envs [] = [[]]
+envs (str:xs) = map (\x -> (str, False) : x) $ envs xs ++ map (\x -> (str, True) : x) (envs xs)
+
+
+test_envs =
     envs ["P", "Q"]
     ==
     [ [ ("P",False)
@@ -66,21 +96,21 @@ test_envs =
     ]
 
 satisfiabila :: Prop -> Bool
-satisfiabila = undefined
- 
+satisfiabila prop =or(map(\x->eval prop x) (envs(variabile prop)))
+
 test_satisfiabila1 = satisfiabila (Not (Var "P") :&: Var "Q") == True
 test_satisfiabila2 = satisfiabila (Not (Var "P") :&: Var "P") == False
 
 valida :: Prop -> Bool
-valida = undefined
+valida p = not(satisfiabila(Not p))
 
 test_valida1 = valida (Not (Var "P") :&: Var "Q") == False
 test_valida2 = valida (Not (Var "P") :|: Var "P") == True
 
 
 echivalenta :: Prop -> Prop -> Bool
-echivalenta = undefined
- 
+echivalenta p1 p2 = valida (p1 :<->: p2) 
+
 test_echivalenta1 =
   True
   ==
